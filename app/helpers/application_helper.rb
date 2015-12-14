51334a1a7f2adfd03ee1342d2a72e32055c3d86d
Zubair Shams <email.to.zubi@gmail.com>
2015-12-14 22:15:52 +0800

    add user cruds and application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class flash_type
+    {
+      :success => 'alert-success',
+      :notice => 'alert-success',
+      :error => 'alert-danger',
+      :alert => 'alert-danger',
+    }[flash_type.to_sym] || flash_type.to_s
+  end
 end
