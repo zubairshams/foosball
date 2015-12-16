6e7bebf6ac58d399d73a0b2bd59508b0408df921
Zubair Shams <email.to.zubi@gmail.com>
2015-12-17 01:45:26 +0800

    unit test for team and user stats

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Team, type: :model do
   it { should have_and_belong_to_many(:users) }
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:users) }
+
+  describe '.stats' do
+    let(:team_one) { create(:team) }
+    let(:team_two) { create(:team) }
+    let(:game_one) { create(:game, team_one: team_one, team_two: team_two, team_one_score: 10, team_two_score: 7) }
+    let(:game_two) { create(:game, team_one: team_one, team_two: team_two, team_one_score: 10, team_two_score: 5) }
+
+    it 'should return teams stats correctly' do
+      create(:match, team_one: team_one, team_two: team_two, games: [game_one, game_two])
+      stats = Team.stats
+      expect(stats.count).to eq 2
+      expect(stats.map(&:name)).to eq [team_one.name, team_two.name]
+      expect(stats.map(&:total_matches)).to eq [1, 1]
+      expect(stats.map(&:total_wins)).to eq [1, 0]
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe User, type: :model do
   it { should have_and_belong_to_many(:teams) }
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
+
+  describe '.stats' do
+    let(:team_one) { create(:team) }
+    let(:team_two) { create(:team) }
+    let(:game_one) { create(:game, team_one: team_one, team_two: team_two, team_one_score: 10, team_two_score: 7) }
+    let(:game_two) { create(:game, team_one: team_one, team_two: team_two, team_one_score: 10, team_two_score: 5) }
+
+    it 'should return users stats correctly' do
+      create(:match, team_one: team_one, team_two: team_two, games: [game_one, game_two])
+      stats = User.stats
+      expect(stats.count).to eq 2
+      expect(stats.map(&:id)).to eq [team_one.users.first.id, team_two.users.first.id]
+      expect(stats.map(&:total_games)).to eq [2, 2]
+      expect(stats.map(&:total_wins)).to eq [2, 0]
+    end
+  end
 end
